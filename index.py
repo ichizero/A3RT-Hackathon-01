c9ed249d04f05ca4dafed8d8c28fd679a98cccbb
ichi <30754019+ichizero@users.noreply.github.com>
2017-10-12 00:21:56 +0900

    add timeout

--- index.py
+++ index.py
@@ -5,6 +5,4 @@ index
 import sys
 from app.main import main
 
-print("DEBUG before main")
 main(sys.argv[1:])
-sys.exit()
--- test/basic.js
+++ test/basic.js
@@ -5,6 +5,7 @@ const app = codecheck.consoleApp(process.env.APP_COMMAND);
 describe("CLIアプリケーション", () => {
 
   it("は実行時に、エラーを発生しない。", () => {
+    this.timeout(10000);
     return app.codecheck().then( result => {
       expect(result.code).to.equal(0,
         　"CLIアプリケーションはステータスコード'0'で終了すべきです。");
